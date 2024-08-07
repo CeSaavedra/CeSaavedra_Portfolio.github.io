e727da3dbfbf9de1be9869c2fe545e055d009c66
Cesar Saavedra <129910837+CeSaavedra@users.noreply.github.com>
2024-08-07 17:58:03 -0400

    Revamped Capstone Project Container UI

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('title-btn').addEventListener('click', function() {
-    var titlePage = document.getElementById('title-page');
-    titlePage.style.opacity = '0';
-    setTimeout(function(){ titlePage.style.display = 'none'; }, 1000);
-});
